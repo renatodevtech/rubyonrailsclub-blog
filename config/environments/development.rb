0ad8e80c44b761be67a550b0c125eb45abc747fd
Renato <renatodevtech@gmail.com>
2024-11-10 11:52:28 -0300

    adicionando autenticacao com devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,4 +82,5 @@ Rails.application.configure do
   config.generators.after_generate do |files|
     system("bundle exec rubocop -A --fail-level=E #{files.shelljoin}", exception: true)
   end
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
